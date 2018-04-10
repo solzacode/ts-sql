3dda711d6f29fad49879b7ac6b43ff6021e490e3
Charles Prakash Dasari <prakash@solza.io>
2018-04-09 22:44:26 -0700

    Work in progress

--- lib/queryValidator.ts
+++ lib/queryValidator.ts
@@ -7,11 +7,11 @@ export enum SymbolState {
     Unresolved
 }
 
-export class SqlAstSymbol implements AstSymbol<ast.SqlNodeType> {
+export class SqlAstSymbol implements AstSymbol<ast.SqlSymbolType> {
     public state: SymbolState;
     public parent?: SqlAstSymbol;
 
-    constructor(public identifier: string, public type: ast.SqlNodeType) {
+    constructor(public identifier: string, public type: ast.SqlSymbolType) {
         this.state = SymbolState.Unresolved;
     }
 }
@@ -29,9 +29,63 @@ export interface ValidationMethod<TNode extends ast.SqlAstNode = ast.SqlAstNode>
     (context: ValidationContext, node: TNode): ValidationContext;
 }
 
-export class QueryValidator extends QueryVisitor {
+export class QueryValidator extends QueryVisitor<ValidationContext> {
     constructor(dialect: ast.SqlDialect, query: ast.SqlRoot, contextBuilder: ContextBuilder = ValidationContext) {
         super(dialect, query, contextBuilder);
+    }
+
+    protected visitSelectStatement: ValidationMethod<ast.SelectStatement> = (context, node) => {
+        context.symbolTable.enterScope();
+        this.visitGenericNode(context, node.query);
+        context.symbolTable.exitScope();
+
+        return context;
+    }
+
+    protected visitQueryExpression: ValidationMethod<ast.QueryExpression> = (context, node) => {
+        if (node.from) {
+            this.visitNode(context, node.from);
+        }
+
+        if (node.elements && node.elements.length) {
+            for (let se of node.elements) {
+                this.visitNode(context, se);
+            }
+        }
+
+        if (node.orderBy) {
+            this.visitNode(context, node.orderBy);
+        }
+
+        if (node.limit) {
+            this.visitNode(context, node.limit);
+        }
+
+        return context;
+    }
+
+    protected visitTableSpec: ValidationMethod<ast.TableSpec> = (context, node) => {
+        let symbol = new SqlAstSymbol(node.tableName.toString(), ast.SqlSymbolType.Table);
+        context.symbolTable.add(symbol);
+        symbol.state = SymbolState.Resolved;
+
+        node.tableName = symbol;
+
+        if (node.partitions) {
+            for (let p of node.partitions) {
+                let partitionField = new SqlAstSymbol(p.toString(), ast.SqlSymbolType.Field);
+                partitionField.parent = symbol;
+                partitionField.state = SymbolState.Resolved;
+
+                context.symbolTable.add(partitionField);
+            }
+        }
+
+        return context;
+    }
+
+    protected visitAliasedTerm: ValidationMethod<ast.AliasedTerm<ast.SqlAstNode>> = (context, node) => {
+        return context;
     }
 }
 
